b0519251e5b653a31d1593db5e5f015955418022
Matej Čief <matej.cief@gmail.com>
2020-11-17 00:39:15 +0100

    Implement moving plaform
    The player moves slowly when on platform... bug or feature?

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float speed;
+    public int activePointIndex;
+    public GameObject movingPoints;
+    public GameObject body;
+    public bool circularMovement;
+    private List<Transform> points = new List<Transform>();
+    private int indexStep = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform[] mp = movingPoints.transform.GetComponentsInChildren<Transform>();
+
+        for (int i = 1; i < mp.Length; i++)
+        {
+            points.Add(mp[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(body.transform.position, points[activePointIndex].position) < 0.1)
+        {
+            if (circularMovement)
+            {
+                activePointIndex = (activePointIndex + 1) % points.Count;
+            }
+            else
+            {
+                if (activePointIndex == points.Count - 1 || activePointIndex == 0)
+                {
+                    indexStep *= -1;
+                }
+                activePointIndex += indexStep;
+            }
+        }
+
+        body.transform.position = Vector3.MoveTowards(body.transform.position, points[activePointIndex].position, speed * Time.deltaTime);
+    }
+}
