9415cc9db34615156de7414f75c07bb8888159d5
Matej Čief <matej.cief@gmail.com>
2020-12-10 12:57:25 +0100

    Add level finish menu

---
+++ Assets/Scripts/UI/LevelFinishMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelFinishMenu : MonoBehaviour
+{
+    public string nextLevelScene;
+
+    private Canvas canvas;
+
+    void Start()
+    {
+        canvas = GetComponent<Canvas>();
+    }
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            canvas.enabled = false;
+        }
+    }
+
+    public void Hide()
+    {
+        Time.timeScale = 1;
+        canvas.enabled = false;
+    }
+
+    public void Display()
+    {
+        Time.timeScale = 0;
+        canvas.enabled = true;
+    }
+
+    public void ResetLevel()
+    {
+        // TODO load stats
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void NextLevel()
+    {
+        // TODO save stats 
+        SceneManager.LoadScene(nextLevelScene);
+    }
+}
