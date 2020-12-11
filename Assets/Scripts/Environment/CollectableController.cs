aa41017f5a8c67957b508719d00a6caade2898c3
Matej Čief <matej.cief@gmail.com>
2020-12-11 12:31:05 +0100

    Add reward animation to a collectable object

--- Assets/Scripts/Environment/CollectableController.cs
+++ Assets/Scripts/Environment/CollectableController.cs
@@ -24,7 +24,9 @@ public class CollectableController : MonoBehaviour, IInteractableObject
     {
         // FIXME need to do proper logic of handling rewards once more types of rewards will come
         StatsUpgrades.Instance.UpgradeStatWithoutPointLoss(reward.ToString().ToLower(), rewardAmount);
-
+        
+        Instantiate(Resources.Load("Animations/Rewards/" + reward.ToString()), (new Vector3(0, 1, 0)), Quaternion.identity, transform);
+        
         animator.SetBool("open", true);
         Destroy(uIAction);
         canvas.SetActive(false);
